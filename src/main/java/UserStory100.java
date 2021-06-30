e0577f3823c41529df241747ed061b231e63c253
Maria <prizmaria@gmail.com>
2021-06-30 19:47:39 -0400

    work1

--- src/main/java/UserStory100.java
+++ src/main/java/UserStory100.java
@@ -2,5 +2,6 @@ public class UserStory100 {
 
     public static void main(String[] args) {
         System.out.println("creating history");
+        System.out.println("you alwys want head to move with your work and be at the lastst commit");
     }
 }
