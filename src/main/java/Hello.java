50bd62d70145a0ba7f9a28780ff9e7cd5d9502c0
Maria <prizmaria@gmail.com>
2021-06-30 19:30:01 -0400

    workign on recap

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,4 +2,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Hello {
 
+    public static void main(String[] args) {
+        System.out.println("recapping like day1");
+    }
 }
