39a4ab1f4625dbe76c467832c514bf2fa9e06ae8
Maria <prizmaria@gmail.com>
2021-06-30 19:32:23 -0400

    more commits

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("recapping like day1");
+        System.out.println("my son is crying and looking for me :-(");
     }
 }
