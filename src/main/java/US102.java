0e1889152e50427d0f5d7a93c0007d55df854e94
Maria <prizmaria@gmail.com>
2021-06-30 21:17:27 -0400

    US102 ready to push

--- src/main/java/US102.java
+++ src/main/java/US102.java
@@ -3,6 +3,6 @@ public class US102 {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("apples");
-
+//ready to push this branch to remote and open pull request
     }
 }
