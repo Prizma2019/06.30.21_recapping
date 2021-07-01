674e1e792a23062608fe08b66a9e23626ad28bc1
Maria <prizmaria@gmail.com>
2021-06-30 20:19:16 -0400

    us101 first commit

--- src/main/java/UserStory100.java
+++ src/main/java/UserStory100.java
@@ -2,7 +2,9 @@ public class UserStory100 {
 
     public static void main(String[] args) {
         System.out.println("creating history");
-        System.out.println("you alwys want head to move with your work and be at the lastst commit");
+        System.out.println("you always want head to move with your work and be at the lastst commit");
         System.out.println("another commit");
+
+        System.out.println("us 101");
     }
 }
